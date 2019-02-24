f177a8dc453d4de1da8ae372da47c0c053dc5e66
shuyangli94 <shuyangli94@users.noreply.github.com>
2019-02-24 15:50:50 -0800

    skill awakening keys

--- scrape_e7x.py
+++ scrape_e7x.py
@@ -3,6 +3,7 @@ import pickle
 import json
 import re
 import string
+import numpy as np
 from datetime import datetime
 from collections import defaultdict
 
@@ -59,20 +60,26 @@ def cache_maps(hero_catalyst_map, sha_map):
 
 NON_CATALYST_RESOURCES = {'molagora', 'molagorago', 'gold'}
 
-def int_dd():
-    return defaultdict(int)
-
 def dict_dd():
     return defaultdict(dict)
+
+def int_arr_len(max_len):
+    return list(map(int, np.zeros(max_len)))
+
+def int_arr_dd(max_len):
+    return defaultdict(lambda: int_arr_len(max_len))
+
+def dict_int_arr_dd(max_len):
+    return defaultdict(lambda: int_arr_dd(max_len))
 
 # Get catalyst usages for a single hero
 def catalyst_usage_for_hero(hero_json):
     # catalyst -> usage list
-    hero_map = defaultdict(int_dd)
+    hero_map = dict_int_arr_dd(10)
 
     # Crawl skills
     skills = hero_json['skills']
-    for skill_level, skill_dict in zip(['s1', 's2', 's3'], skills):
+    for skill_level, skill_dict in enumerate(skills):
         enhancement_levels = skill_dict['enhancement']
 
         # Crawl each enhancement level
@@ -81,17 +88,16 @@ def catalyst_usage_for_hero(hero_json):
                 resource = resource_dict['item']
                 # if it's a catalyst, update with the skill level and quantity needed
                 if resource not in NON_CATALYST_RESOURCES:
-                    hero_map[resource][skill_level] += resource_dict['qty']
+                    hero_map[resource]['Skills'][skill_level] += resource_dict['qty']
 
     # Crawl awakenings
     for awakening_dict in hero_json['awakening']:
-        awakening_key = 'awakening_{}'.format(awakening_dict['rank'])
         for resource_dict in awakening_dict['resources']:
             resource = resource_dict['item']
 
             # If it's a catalyst, update with awakening level and quantity needed
             if 'rune' not in resource:
-                hero_map[resource][awakening_key] += resource_dict['qty']
+                hero_map[resource]['Awakening'][awakening_dict['rank'] - 1] += resource_dict['qty']
 
     return hacky_dd_to_dict(hero_map)
 
@@ -133,16 +139,16 @@ def create_catalyst_map(hero_catalyst_map):
 
     catalyst:
         hero:
-            usage:
-                quantity
+            Skill/Awakening:
+                list of quantities
 
     e.g.
-    fused-nerve:
-        achates:
-            s1: 2
-            s2: 2
-            s3: 2
-            awakening_6: 8
+    demon-blood-gem:
+        Rin:
+            Skills:
+                [2, 2, 1]
+            Awakening:
+                [0, 0, 0, 0, 0, 8]
     '''
     start = datetime.now()
 
@@ -164,7 +170,7 @@ def create_catalyst_map(hero_catalyst_map):
 USAGE:
 
 import json
-from scrape_e7x import get_maps, update_maps, create_catalyst_map, cache_maps, format_catalyst_name
+from scrape_e7x import get_maps, update_maps, create_catalyst_map, cache_maps
 
 hero_catalyst_map, sha_map = update_maps(*get_maps())
 catalyst_map = create_catalyst_map(hero_catalyst_map)
