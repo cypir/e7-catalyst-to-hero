20b9d078d1d3f7b3d5638cf90e0c45edc05dbf31
Alex Nguyen <alex@cypir.com>
2019-02-25 00:46:59 +0000

    basic site

--- src/App.js
+++ src/App.js
@@ -1,25 +1,67 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import TextField from "@material-ui/core/TextField";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import cataMap from "./e7_cata_to_hero_map.json";
+import slug from "slug";
+import { ListItemText } from "@material-ui/core";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import CheckIcon from "@material-ui/icons/CheckCircle";
+import CloseIcon from "@material-ui/icons/Close";
+import Typography from "@material-ui/core/Typography";
 
 class App extends Component {
+  state = {
+    search: "",
+    results: []
+  };
+
+  onSearchChange = e => {
+    this.setState({ search: e.target.value });
+  };
+
+  //TODO: enable search for catalyst / fuzzy search
+  //TODO: first entry should show list of catalysts
+
+  onSubmit = e => {
+    e.preventDefault();
+
+    let searchKey = slug(this.state.search);
+    let results = cataMap[searchKey];
+
+    let heroesArray = Object.keys(results).map(key => {
+      return { hero: key, ...results[key] }; //create an array where we put the key into the contents of the array
+    });
+    this.setState({ results: heroesArray });
+  };
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>
+        <form onSubmit={this.onSubmit}>
+          <TextField
+            label="Catalyst"
+            name="catalyst"
+            value={this.state.search}
+            onChange={this.onSearchChange}
+          />
+        </form>
+        <List component="nav">
+          {this.state.results.map((result, index) => {
+            console.log(result);
+            return (
+              <ListItem key={index} button>
+                <ListItemText>{result.hero}</ListItemText>
+                <ListItemSecondaryAction>
+                  {result.Awakening ? <CheckIcon /> : <CloseIcon />}
+                  Awakening
+                  {result.Skills ? <CheckIcon /> : <CloseIcon />}
+                  Skill Enhance
+                </ListItemSecondaryAction>
+              </ListItem>
+            );
+          })}
+        </List>
       </div>
     );
   }
